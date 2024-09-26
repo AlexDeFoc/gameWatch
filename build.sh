4659fe4e1b6386b73e669c737ab198e044b4fefa
AlexDeFoc <alex.de.foc@gmail.com>
2024-09-26 14:55:42 +0300

    changed to cmake!

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-libs="src"
-includes="$libs/main.cpp $libs/item.cpp"
-clang++ -static -I"$libs" $includes -o "main.exe"
+#!/bin/sh
+cd build ; make
---
+++ configure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cmake -DCMAKE_CXX_STANDARD=17 -S . -B build -G "Unix Makefiles"
